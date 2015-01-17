9bc77791a4ee90bd109089220bf9ae1d4776cbad
Adam Tong <adam.tong@gmail.com>
2015-01-17 09:09:38 +0100

    Initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,66 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# -----------------------------------------------------------------------------
+# R Programming
+# Programming Assignment 2: Lexical Scoping
+#
+# The pair of functions in this file take advantage of the scoping rules of 
+# the R language to cache the inverse of a matrix.
+# -----------------------------------------------------------------------------
 
 makeCacheMatrix <- function(x = matrix()) {
+    # Creates a special "matrix" object that can cache its inverse.
+    #
+    # Args:
+    #   x: matrix whose inverse is to be computed
+    #
+    # Returns:
+    #   A list with the following methods:
+    #     set(), get(), setInverse(), getInverse()
+    #
+    
+    # The cached inverse of matrix x
+    xi <- NULL
 
+    list(
+        set = function(y) {
+            x <<- y
+            xi <<- NULL
+        },
+        get = function() { x },
+        setInverse = function(inv) { xi <<- inv },
+        getInverse = function() { xi } )
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    #
+    # Computes the inverse of the special "matrix" returned by makeCacheMatrix.
+    # If the inverse has already been calculated, then cacheSolve will retrieve 
+    # the inverse from the cache.
+    #
+    # Args:
+    #   x: special matrix created by makeCacheMatrix.
+    #
+    # Returns:
+    #   The inverse of the matrix x
+    #
+
+    # Look for the inverse of 'x' in the cache
+    xi <- x$getInverse()
+    if (!is.null(xi)) {
+        message("returning cached inverse")
+        return(xi)
+    }
+
+    # Solve for the inverse
+    data <- x$get()
+    xi <- solve(data, ...)
+
+    # Store inverse in cache and return result
+    x$setInverse(xi)
+    xi
 }
+
+# -----------------------------------------------------------------------------
+# ChangeLog
+# 2015-01-17  Adam Tong  <adam.tong@gmail.com>
+#   * Created based on example in assignment
+# -----------------------------------------------------------------------------
